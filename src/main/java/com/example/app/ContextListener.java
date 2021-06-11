16f497ce0f78fdc76c9fe93b2485bc1685db358d
sauvageb <sauvageboris.pro@gmail.com>
2021-06-11 10:41:54 +0200

    feat(Listener): listening servletContext

---
+++ src/main/java/com/example/app/ContextListener.java
@@ -0,0 +1,21 @@
+package com.example.app;
+
+import com.example.app.dao.jpa.PersistenceManager;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        PersistenceManager.getConnection();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        PersistenceManager.closeConnection();
+    }
+}
