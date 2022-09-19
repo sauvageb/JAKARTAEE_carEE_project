f1c44f15c10cad8f85624af634deb24ec66bc36c
sauvageb <sauvageboris.pro@gmail.com>
2022-09-19 16:54:01 +0200

    starting implement jpa and jdbc dao layers

--- src/main/java/com/example/app/dao/DaoFactory.java
+++ src/main/java/com/example/app/dao/DaoFactory.java
@@ -1,7 +1,8 @@
 package com.example.app.dao;
 
 
-import com.example.app.dao.memory.MemoryCarDao;
+import com.example.app.dao.jpa.JpaCarDao;
+import com.example.app.dao.jpa.PersistenceManager;
 import com.example.app.dao.memory.MemoryCategoryDao;
 
 public final class DaoFactory {
@@ -10,11 +11,14 @@ public final class DaoFactory {
     }
 
     public static CarDao getCarDao() {
-        return new MemoryCarDao();
+//        return new MemoryCarDao();
+//        return new JdbcCarDao(ConnectionManager.getConnection());
+        return new JpaCarDao(PersistenceManager.getConnection());
     }
 
 
     public static CategoryDao getCategoryDao() {
         return new MemoryCategoryDao();
+        //TODO : return new JPA CategoryDAO
     }
 }
